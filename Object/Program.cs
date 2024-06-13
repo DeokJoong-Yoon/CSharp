a1717e86e66719d6cbd511a1a335cd49ad719797
DESKTOP-4N15ASS\Yoon <ykdj92@naver.com>
2024-06-14 00:15:19 +0900

    20240614_home

---
+++ Object/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = 123;
+            object b = 3.14159234849393294389m;
+            object c = true;
+            object d = "안녕하세요";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
