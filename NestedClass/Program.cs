1a7cac48cd2ca093c7d86905e6deb5de3fce2ea4
DESKTOP-4N15ASS\Yoon <ykdj92@naver.com>
2024-07-21 22:41:43 +0900

    20240721_home

---
+++ NestedClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace NestedClass
+{
+    class Configuration
+    {
+        List<ItemValue> listCOnfig = new List<ItemValue>();
+
+        public void SetConfig(string item, string value)
+        {
+            
+        }
+    }
+
+    private class ItemValue
+    {
+
+    }
+}
