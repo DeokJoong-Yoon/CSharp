5443ad078f9c2f2dd28cc03b34cd8f28e4cb62d0
DESKTOP-4N15ASS\Yoon <ykdj92@naver.com>
2025-02-04 22:56:45 +0900

    home

---
+++ AnonymousType/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AnonymousType
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            var a = new { Name = "박상현", Age = 123 };
+            Console.WriteLine($"Name: {a.Name}, Age : {a.Age}");
+        }
+    }
+}
---
+++ RecordComp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RecordComp
+{
+    class CTransaction
+    {
+        public string From { get; init; }
+        public string To { get; init; }
+        public int Amount { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10} -> {To,-10} : ${Amount}";
+        }
+    }
+
+    record RTransaction
+    {
+        public string From { get; init; }
+        public string To { get; init; }
+        public int Amount { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10} -> {To,-10} : ${Amount}";
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            CTransaction trA = new CTransaction { From = "Aclice", To = "Bob", Amount = 100 };
+            CTransaction trB = new CTransaction { From = "Alice", To = "Bob", Amount = 100 };
+
+            Console.WriteLine(trA);
+            Console.WriteLine(trB);
+            Console.WriteLine($"trA eqals to trB : {trA.Equals(trB)}");
+        }
+    }
+}
